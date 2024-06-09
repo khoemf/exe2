9e5b274c49a09005f9518f684fdd9d0ab9aacd65
khoemf <khoemf@gmail.com>
2024-06-09 23:37:00 +0800

    commit

--- u_FinalCode/08 Refs Portals/08-forwarding-refs/src/components/Player.jsx
+++ u_FinalCode/08 Refs Portals/08-forwarding-refs/src/components/Player.jsx
@@ -1,20 +1,18 @@
-import { useState ,useRef } from "react";
+import { useState, useRef } from 'react';
 
 export default function Player() {
   const playerName = useRef();
-  const [enterdPlayerName , setEnteredPlayerName] = useState('');
 
-  //const [submitted, setSubmitted] = useState(false);
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
-  
-  function handleClick(){
+  function handleClick() {
     setEnteredPlayerName(playerName.current.value);
     playerName.current.value = '';
   }
 
   return (
     <section id="player">
-      <h2>Welcome { enterdPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
